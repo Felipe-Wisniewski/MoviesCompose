d8185c497b9388e20099d62ed7fc3245f166b210
Felipe Wisniewski <felipewisniewski@gmail.com>
2025-02-13 20:58:58 -0300

    fixed landscape view in player screen

--- ui/src/main/java/com/wisnitech/moviescompose/ui/player/YouTubePlayerScreen.kt
+++ ui/src/main/java/com/wisnitech/moviescompose/ui/player/YouTubePlayerScreen.kt
@@ -1,19 +1,42 @@
 package com.wisnitech.moviescompose.ui.player
 
+import android.app.Activity
+import android.content.pm.ActivityInfo
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.options.IFramePlayerOptions
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
 @Composable
 fun YouTubePlayerScreen(
     movieKey: String
 ) {
+    val activity = LocalContext.current as Activity
     val lifecycleOwner = LocalLifecycleOwner.current
+
+    LaunchedEffect(movieKey) {
+        activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+    }
+
+    DisposableEffect(Unit) {
+        onDispose {
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+        }
+    }
+
+
+    val iFramePlayerOptions = IFramePlayerOptions.Builder()
+        .controls(1)
+        .fullscreen(1)
+        .build()
 
     AndroidView(
         modifier = Modifier.fillMaxSize(),
@@ -21,11 +44,15 @@ fun YouTubePlayerScreen(
             YouTubePlayerView(context = context).apply {
                 lifecycleOwner.lifecycle.addObserver(this)
 
-                addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+                enableAutomaticInitialization = false
+
+                val youTubePlayListener = object : AbstractYouTubePlayerListener() {
                     override fun onReady(youTubePlayer: YouTubePlayer) {
                         youTubePlayer.loadVideo(movieKey, 0f)
                     }
-                })
+                }
+
+                initialize(youTubePlayListener, iFramePlayerOptions)
             }
         }
     )
